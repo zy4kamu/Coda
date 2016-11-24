00a1673b02a0bfa446d663211ab2352710cc9e96
stepanmuz <stepan.muzychka@gmail.com>
2016-11-24 10:30:45 +0100

    have created common_cmake.cmake

---
+++ Coda/src/common_includes.cmake
@@ -0,0 +1,9 @@
+get_filename_component(project_name ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+string(REPLACE " " "_" project_name ${project_name})
+string(TOLOWER ${project_name} project_name)
+
+file(GLOB sources "*.cpp" "*/*.cpp" "*/*/*.cpp")
+file(GLOB headers "*.h" "*/*.h" "*/*/*.h")
+file(GLOB sourceheaders "*.hpp" "*/*.hpp" "*/*/*.hpp")
+
+ADD_DEFINITIONS("-Wall" "-fPIC")
