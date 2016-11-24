7f600999158155d5a2f88c8075ebcb6b3d4fa3f6
stepanmuz <stepan.muzychka@gmail.com>
2016-11-24 10:47:47 +0100

    changed cmake in optimization

--- Coda/src/common_includes.cmake
+++ Coda/src/common_includes.cmake
@@ -2,7 +2,7 @@ get_filename_component(project_name ${CMAKE_CURRENT_SOURCE_DIR} NAME)
 string(REPLACE " " "_" project_name ${project_name})
 string(TOLOWER ${project_name} project_name)
 
-file(GLOB sources "*.cpp" "*/*.cpp" "*/*/*.cpp")
+file(GLOB sources "*.c" "*/*.c" "*/*/*.c" "*.cpp" "*/*.cpp" "*/*/*.cpp")
 file(GLOB headers "*.h" "*/*.h" "*/*/*.h")
 file(GLOB sourceheaders "*.hpp" "*/*.hpp" "*/*/*.hpp")
 
